336a0dc69bad5aab59e1f2a0ea0e710aa34e8a3d
eloyfayffer <eefayffer@gmail.com>
2022-11-05 19:12:05 -0500

    Ejercicio 6_7_8

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,25 +11,28 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
-
+      this.nombre = nombre;
+      this.edad  = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
     }
 
     addFriend(nombre, edad) {
       // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
-
+      this.amigos.push({"nombre" : nombre , "edad" : edad});
     }
 
     addHobby(hobby) {
       // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
-
+       
+      this.hobbies.push(hobby);
     }
+
     getFriends() {
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
@@ -38,16 +41,24 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
-
+      var cadena =[];  
+      for (let i=0; i < this.amigos.length; i++){
+          cadena[i] = this.amigos[i].nombre;
+      }
+      return cadena;
     }
 
     getHobbies() {
       // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
+      
       // Tu código aca:
-
+      var cadena_get = [];  
+      for (let i=0; i < this.hobbies.length ; i++){
+        cadena_get[i] = this.hobbies[i];
+      }
+      return cadena_get;
     }
 
     getPromedioEdad() {
@@ -66,7 +77,11 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
-
+      var suma = 0;
+      for (var i=0; i < this.amigos.length ; i++){
+        suma = suma + this.amigos[i].edad;
+      }
+      return suma / this.amigos.length;
     }
   };
 
